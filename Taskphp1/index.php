bdc9891f1fef3722e21344c49c01570ef5b0d2ad
ReemaAbdulla <74876644+ReemaAbdulla@users.noreply.github.com>
2020-11-30 10:56:15 +0200

    Add files via upload

---
+++ Taskphp1/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+//to count tne number of words in any string using str_word_count()
+echo "The number of words in the string 'Hello MOL' =  ";
+echo str_word_count("Hello MOL")."<br>";
+echo "<br>";
+
+//to reverse a string using function strrev()
+echo "The reverse of a string 'Hello MOL' is :    ";
+echo strrev("Hello MOL")."<br>";
+echo "<br>";
+
+//to find the position of any word in a string using function strpos()
+echo "The position of the word 'MOL'  in the string 'Hello MOL' =  ";
+echo strpos("Hello MOL","MOL")."<br>";
+
+
+?>
+
